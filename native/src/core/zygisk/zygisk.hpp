017672eddbb7e70e7ca1ef0150301b432ac55e83
SUMUS <ljymyys@outlook.com>
2024-02-15 17:18:59 +0800

    test zygisk log

--- native/src/core/zygisk/zygisk.hpp
+++ native/src/core/zygisk/zygisk.hpp
@@ -28,8 +28,8 @@ enum : int {
 #endif
 
 // Extreme verbose logging
-#define ZLOGV(...) ZLOGD(__VA_ARGS__)
-//#define ZLOGV(...) (void*)0
+//#define ZLOGV(...) ZLOGD(__VA_ARGS__)
+#define ZLOGV(...) (void*)0
 
 void hook_functions();
 void hookJniNativeMethods(JNIEnv *env, const char *clz, JNINativeMethod *methods, int numMethods);
